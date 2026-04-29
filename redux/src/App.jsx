d2256f010ea80309ab00dc0ddd8aed78f60d0204
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-29 22:53:22 +0530

    r

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,1 +1,17 @@
- 
+import React from 'react'
+
+const App = () => {
+
+  function addbook(){
+    
+  }
+  return (
+    <div>
+
+      <button onClick={addbook}>add book</button>
+      
+    </div>
+  )
+}
+
+export default App
--- redux/src/store/slice/socialslice.js
+++ redux/src/store/slice/socialslice.js
@@ -1,23 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
-import socialslice from '../slice/socialslice.js'
 
-
-
-const socialslice=createSlice({
-    name:'social',
-    initialState:{
-        books:[]
+const socialSlice = createSlice({
+    name: 'social',
+    initialState: {
+        books: []
     },
-    reducers:{
-        addbook:(state,action) =>{
-            console.log("addbook function",action.payload)
-            state.books.push(action.payload)
-
+    reducers: {
+        addBook: (state, action) => {
+            state.books.push(action.payload);
         }
     }
-})
+});
 
-
-export const {addbook}=socialslice.actions
-
-export default socialslice.reducer;
+export const { addBook } = socialSlice.actions;
+export default socialSlice.reducer;
